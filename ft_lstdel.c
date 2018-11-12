6169b4d43e53a25a73ce05ac9d444d7333dedf09
Jae seok LEE <jaelee@e3r5p8.42.fr>
2018-11-12 17:01:57 +0100

    function protection modified

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:49:37 by jaelee            #+#    #+#             */
-/*   Updated: 2018/11/12 16:39:53 by jaelee           ###   ########.fr       */
+/*   Updated: 2018/11/12 17:01:30 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list	*tmp;
 
-	tmp = *alst;
 	if (!(alst && del))
 		return ;
+	tmp = (*alst);
 	while ((*alst) != (NULL))
 	{
 		tmp = (*alst)->next;
